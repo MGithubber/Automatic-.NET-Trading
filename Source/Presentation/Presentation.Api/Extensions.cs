a72058c9d5205f7f5e7f331a2603e01d08ba0fed
MGithubber <mircea.ionescu3000@gmail.com>
2022-12-10 14:30:29 +0200

    Added a new presentation ASP.NET Core project

---
+++ Source/Presentation/Presentation.Api/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Presentation.Api;
+
+public static class Extensions
+{
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // // to do // //
+        
+    }
+
+    public static void MapEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/weatherforecast", () => { })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+    }
+}
